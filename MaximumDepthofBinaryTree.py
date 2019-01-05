603dd0d4f730d1027528a5b463077aed602afbee
wyiyun95 <46390811+wyiyun95@users.noreply.github.com>
2019-01-04 23:15:56 -0500

    Update MaximumDepthofBinaryTree.py

--- MaximumDepthofBinaryTree.py
+++ MaximumDepthofBinaryTree.py
@@ -1,5 +1,5 @@
-“”“Given a binary tree, find its maximum depth.
-   The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.”“”
+"""Given a binary tree, find its maximum depth.
+   The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node."""
 
 class Solution:
     def maxDepth(self, root):
